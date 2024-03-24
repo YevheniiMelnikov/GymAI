9152d968a82f1116d825415663d1250d0c2bca2b
Evhenii Melnykov <evgeniy@workmail.com>
2024-03-24 21:48:01 +0200

    DB settings updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,12 +80,11 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "password",
-        "HOST": "db",
-        # "HOST": "localhost",
-        "PORT": 5432,
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
         "TEST": {
             "NAME": "test_db",
         },
