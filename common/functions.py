a785d4ef217758c4e8c2956eea8fdd8a7c1c7522
Evhenii Melnykov <evgeniy@workmail.com>
2024-04-20 19:58:03 +0300

    Functions refactored

--- common/functions.py
+++ common/functions.py
@@ -18,71 +18,71 @@ bot = Bot(os.environ.get("BOT_TOKEN"))
 BACKEND_URL = os.environ.get("BACKEND_URL")
 
 
-async def show_main_menu(message: Message, state: FSMContext, lang: str) -> None:
+async def show_main_menu(message: Message, state: FSMContext, lang: str):
     profile = user_service.session.get_current_profile_by_tg_id(message.from_user.id)
-    if profile.status == "client":
-        await state.set_state(States.client_menu)
-        await message.answer(
-            text=translate(MessageText.main_menu, lang=lang),
-            reply_markup=client_menu_keyboard(lang),
-        )
-    elif profile.status == "coach":
-        await state.set_state(States.coach_menu)
-        await message.answer(
-            text=translate(MessageText.main_menu, lang=lang),
-            reply_markup=coach_menu_keyboard(lang),
-        )
+    menu = client_menu_keyboard if profile.status == "client" else coach_menu_keyboard
+    await state.set_state(States.client_menu if profile.status == "client" else States.coach_menu)
+    await message.answer(text=translate(MessageText.main_menu, lang=lang), reply_markup=menu(lang))
 
 
 async def register_user(message: Message, state: FSMContext, data: dict) -> None:
     await state.update_data(email=message.text)
-    if profile := await user_service.sign_up(
+    profile = await user_service.sign_up(
         username=data["username"],
         password=data["password"],
         email=message.text,
         status=data["account_type"],
         language=data["lang"],
-    ):
-        logger.info(f"User {message.from_user.id} registered")
-        auth_token = await user_service.log_in(username=data["username"], password=data["password"])
+    )
 
-        if auth_token:
-            logger.info(f"User {message.from_user.id} logged in")
-            user_service.session.set_profile(profile, auth_token, message.from_user.id)
-            await message.answer(text=translate(MessageText.registration_successful, lang=data["lang"]))
-            await show_main_menu(message, state, data["lang"])
-        else:
-            await handle_registration_failure(message, state, data["lang"])
-    else:
+    if not profile:
+        logger.error(f"Registration failed for user {message.from_user.id}")
         await handle_registration_failure(message, state, data["lang"])
+        return
+
+    logger.info(f"User {message.from_user.id} registered")
+    auth_token = await user_service.log_in(username=data["username"], password=data["password"])
+
+    if not auth_token:
+        logger.error(f"Login failed for user {message.from_user.id} after registration")
+        await handle_registration_failure(message, state, data["lang"])
+        return
+
+    logger.info(f"User {message.from_user.id} logged in")
+    user_service.session.set_profile(profile, auth_token, message.from_user.id)
+    await message.answer(text=translate(MessageText.registration_successful, lang=data["lang"]))
+    await show_main_menu(message, state, data["lang"])
 
 
 async def sign_in(message: Message, state: FSMContext, data: dict) -> None:
-    if auth_token := await user_service.log_in(username=data["username"], password=message.text):
-        logger.info(f"User {message.from_user.id} logged in")
-        if profile := await user_service.get_profile_by_username(data["username"], auth_token):
-            await state.update_data(login_attempts=0)
-            user_service.session.set_profile(profile, auth_token, telegram_id=message.from_user.id)
-            logger.info(f"Profile {profile.id} set for user {message.from_user.id}")
-            await message.answer(text=translate(MessageText.signed_in, lang=data["lang"]))
-            await show_main_menu(message, state, data["lang"])
-            await message.delete()
-        else:
-            await message.answer(text=translate(MessageText.unexpected_error, lang=data["lang"]))
-            await state.set_state(States.username)
-            await message.answer(text=translate(MessageText.username, lang=data["lang"]))
-            await message.delete()
-    else:
+    auth_token = await user_service.log_in(username=data["username"], password=message.text)
+    if not auth_token:
         attempts = data.get("login_attempts", 0) + 1
+        await state.update_data(login_attempts=attempts)
         if attempts >= 3:
             await message.answer(text=translate(MessageText.reset_password_offer, lang=data["lang"]))
-            return
+        else:
+            await message.answer(text=translate(MessageText.invalid_credentials, lang=data["lang"]))
+            await state.set_state(States.username)
+            await message.answer(text=translate(MessageText.username, lang=data["lang"]))
+        await message.delete()
+        return
 
-        await state.update_data(login_attempts=attempts)
-        await message.answer(text=translate(MessageText.invalid_credentials, lang=data["lang"]))
+    logger.info(f"User {message.from_user.id} logged in")
+    profile = await user_service.get_profile_by_username(data["username"], auth_token)
+    if not profile:
+        await message.answer(text=translate(MessageText.unexpected_error, lang=data["lang"]))
         await state.set_state(States.username)
         await message.answer(text=translate(MessageText.username, lang=data["lang"]))
         await message.delete()
+        return
+
+    await state.update_data(login_attempts=0)
+    user_service.session.set_profile(profile, auth_token, telegram_id=message.from_user.id)
+    logger.info(f"Profile {profile.id} set for user {message.from_user.id}")
+    await message.answer(text=translate(MessageText.signed_in, lang=data["lang"]))
+    await show_main_menu(message, state, data["lang"])
+    await message.delete()
 
 
 async def handle_registration_failure(message: Message, state: FSMContext, lang: str) -> None:
@@ -98,28 +98,17 @@ def validate_birth_date(date_str: str) -> bool:
         return False
 
     year, month, day = map(int, date_str.split("-"))
-    if year < 1900 or year > 2100:
+    if not (1900 <= year <= 2100 and 1 <= month <= 12):
         return False
 
-    if month < 1 or month > 12:
+    if (month in [4, 6, 9, 11] and day > 30) or (
+        month == 2 and day > (29 if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0 else 28)
+    ):
         return False
 
-    if day < 1 or day > 31:
-        return False
-
-    if month in [4, 6, 9, 11] and day > 30:
-        return False
-
-    if month == 2:
-        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-            if day > 29:
-                return False
-        elif day > 28:
-            return False
-
-    return True
+    return 1 <= day <= 31
 
 
-def validate_email(email):
+def validate_email(email: str) -> bool:
     pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$"
     return bool(re.match(pattern, email))
