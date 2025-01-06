1c779b753cf84298588c5da2c9723c0756ea0c9d
Yevhenii Melnykov <yevhenii.melnykov@codeit.local>
2025-01-06 16:07:58 +0200

    Singleton decorator fixed

--- common/decorators.py
+++ common/decorators.py
@@ -4,7 +4,9 @@ def singleton(cls):
     class Wrapper(cls):
         def __new__(cls, *args, **kwargs):
             if cls not in instances:
-                instances[cls] = super(Wrapper, cls).__new__(cls)
+                instances[cls] = super(Wrapper, cls).__new__(cls, *args, **kwargs)
             return instances[cls]
 
+    Wrapper.__name__ = cls.__name__
+    Wrapper.__module__ = cls.__module__
     return Wrapper
