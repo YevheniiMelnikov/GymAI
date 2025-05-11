c40383a8513e229fdeff181037a5cabe6f64f477
Yevhenii Melnykov <yevhenii.melnykov@codeit.pro>
2025-05-11 20:08:32 +0300

    Healthcheck added

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,15 @@
+from django.http import JsonResponse
 from django.urls import include, path
 
 from apps.payments.views import PaymentWebhookView
 
 
+def healthcheck_view(_):
+    return JsonResponse({"status": "ok"})
+
+
 urlpatterns = [
+    path("health/", healthcheck_view, name="healthcheck"),
     path("payment-webhook/", PaymentWebhookView.as_view(), name="payment-webhook"),
     path("api/v1/", include("apps.profiles.urls")),
     path("api/v1/", include("apps.payments.urls")),
