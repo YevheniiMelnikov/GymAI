c2e49cb541e069f07fec7b0fbf600813e8aceceb
Yevhenii <evgeniy@workmail.com>
2025-07-12 18:45:00 +0300

    Expose service proxies

--- core/services/__init__.py
+++ core/services/__init__.py
@@ -1,5 +1,6 @@
 from core.services.gdrive_loader import GDriveDocumentLoader
 from core.services.gstorage_service import GCStorageService, ExerciseGIFStorage
+from core.services.internal import APIService, ProfileService, WorkoutService
 
 avatar_manager = GCStorageService("coach_avatars")
 gif_manager = ExerciseGIFStorage("exercises_guide")
@@ -8,4 +9,7 @@ __all__ = [
     "avatar_manager",
     "gif_manager",
     "GDriveDocumentLoader",
+    "APIService",
+    "ProfileService",
+    "WorkoutService",
 ]
